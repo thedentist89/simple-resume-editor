9933c6d6a68e6cddd43a8a3d8d803eb39eed3ed3
Mourad Aouinat <mourad.eyes@gmail.com>
2020-03-06 13:22:25 +0100

    Make Banner Appear on Mouse over

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,25 @@ import { Link } from "@reach/router";
 
 const Banner = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isBannerOpen, setIsBannerOpen] = useState(false);
+
+  const showBanner = () => {
+    setIsBannerOpen(!isBannerOpen);
+  };
 
   return (
     <>
-      <div className="flex items-center justify-between p-4 bg-white rounded shadow relative">
+      <div
+        className="w-full h-6 left-0 fixed top-0"
+        onMouseOver={() => setIsBannerOpen(true)}
+      ></div>
+      <div
+        className={`flex z-50 items-center justify-between p-4 bg-white rounded shadow fixed top-0 w-5/6 mx-auto mt-8 transition ease-out duration-500 transform -translate-x-1/2 ${
+          isBannerOpen ? "" : "-translate-y-24"
+        }`}
+        style={{ left: "50%" }}
+        onMouseLeave={showBanner}
+      >
         <h1 className="text-indigo-600 font-bold">
           Simple Resume Editor v0.0.1
         </h1>
@@ -19,12 +34,19 @@ const Banner = () => {
           <Options className="h-8 w-8" />
         </button>
         {isMenuOpen && (
-          <div className="rounded bg-white shadow absolute right-0 top-0 mt-20 overflow-hidden py-2 z-50">
+          <div
+            className="rounded bg-white shadow absolute right-0 top-0 mt-20 overflow-hidden py-2 z-50"
+            onMouseOver={() => setIsBannerOpen(true)}
+            onMouseleave={() => setIsBannerOpen(false)}
+          >
             {window.location.pathname === "/editor" ? (
               <Link
                 to="/"
                 className="list-none px-16 py-2 hover:bg-purple-200 cursor-pointer"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                  setIsBannerOpen(false);
+                }}
               >
                 Save
               </Link>
@@ -32,7 +54,10 @@ const Banner = () => {
               <Link
                 to="editor"
                 className="list-none px-16 py-2 hover:bg-purple-200 cursor-pointer"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                onClick={() => {
+                  setIsMenuOpen(!isMenuOpen);
+                  setIsBannerOpen(false);
+                }}
               >
                 Edit
               </Link>
