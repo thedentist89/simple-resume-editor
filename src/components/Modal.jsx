cea4fc6c3727a27e39d81bb8b2f131b98c368043
Mourad Aouinat <mourad.eyes@gmail.com>
2020-03-16 01:39:14 +0100

    add animations to the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { createPortal } from "react-dom";
 import { ReactComponent as Close } from "../assets/close.svg";
+import { useSpring, animated } from "react-spring";
 
 const modalRoot = document.getElementById("modal-root");
 
@@ -11,11 +12,14 @@ export function useModal() {
 }
 
 const Modal = ({ children, toggle }) => {
-  const closeOnEsc = e => {
-    if (e.keyCode === 27) {
-      toggle();
-    }
-  };
+  const closeOnEsc = useCallback(
+    e => {
+      if (e.keyCode === 27) {
+        toggle();
+      }
+    },
+    [toggle]
+  );
 
   useEffect(() => {
     const el = document.createElement("div");
@@ -26,7 +30,13 @@ const Modal = ({ children, toggle }) => {
       modalRoot.removeChild(el);
       document.removeEventListener("keydown", closeOnEsc);
     };
-  }, []);
+  }, [closeOnEsc]);
+
+  const animation = useSpring({
+    to: { opacity: 1, transform: "scale(1)" },
+    from: { opacity: 0, transform: "scale(0.2)" },
+    config: { duration: 200 }
+  });
 
   return createPortal(
     <div
@@ -34,7 +44,10 @@ const Modal = ({ children, toggle }) => {
       style={{ backgroundColor: "rgba(200, 200, 200, .3)" }}
       aria-modal="true"
     >
-      <div className="z-50 bg-white rounded-md py-6 px-4 shadow-xl mx-auto w-1/2">
+      <animated.div
+        className="z-50 bg-white rounded-md py-6 px-4 shadow-xl mx-auto w-1/2"
+        style={animation}
+      >
         <div className="text-right">
           <button
             className="rounded-full bg-gray-300 hover:bg-gray-400"
@@ -43,8 +56,10 @@ const Modal = ({ children, toggle }) => {
             <Close className="text-gray-800 w-6 h-6" />
           </button>
         </div>
+
         {children}
-      </div>
+      </animated.div>
+
       <div
         role="button"
         className="absolute inset-0 h-full w-full cursor-default"
